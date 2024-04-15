7228272748dfd7a14fd911a0e205884fa539703b
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-15 18:03:38 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# electon-learn-showcase
+
+This is an electron app that I built while taking an Electron course.
---
+++ src/main/index.ts
@@ -0,0 +1,45 @@
+import { app, BrowserWindow } from "electron";
+import { join } from "path";
+
+const createWindow = () => {
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		show: false,
+		webPreferences: {
+			preload: join(__dirname, "preload.js"),
+		},
+	});
+
+	if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+		mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+	} else {
+		mainWindow.loadFile(
+			join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+		);
+	}
+
+	mainWindow.once("ready-to-show", () => {
+		mainWindow.show();
+		mainWindow.focus();
+		console.log("ready-to-show");
+	});
+
+	// mainWindow.webContents.openDevTools({
+	// 	mode: "detach",
+	// });
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+app.on("activate", () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
+});
